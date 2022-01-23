4d923b9e4eee30a0e0680cac406b2382feb27f55
Moinak Banerjee <32922277+moinak878@users.noreply.github.com>
2022-01-23 19:52:30 +0530

    Update rec_vedz.cpp

--- topicwise/recursion/rec_vedz.cpp
+++ topicwise/recursion/rec_vedz.cpp
@@ -1,27 +1,25 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 /*
 123454321 (n=5)
  2345432    (n=4)
 
 */
-void rec_pat(int n , int p  ){
+void rec_pat(int n , int p ){
     if(n==1){
-        for (int k = 0; k < p-1;k++)
-            cout << " ";
+        cout<<setw(p);
+        setfill(' ');
         cout << p << "\n";
         return;
     }
-    for (int k = n; k < p; k++)
-        cout << " ";
-    for (int i = p - n + 1; i <= p;i++)
+    cout << setw(p-n+1) << setfill(' ');
+    for (int i = p - n + 1; i <= p; i++)
         cout << i;
     for (int i = p - 1; i >= p-n+1;i--)
         cout << i;
     cout << "\n";
     rec_pat(n - 1, p);
-    for (int k = n; k < p; k++)
-        cout << " ";
+    cout << setw(p-n+1) << setfill(' ');
     for (int i = p - n + 1; i <= p;i++)
         cout << i;
     for (int i = p - 1; i >= p-n+1;i--)
